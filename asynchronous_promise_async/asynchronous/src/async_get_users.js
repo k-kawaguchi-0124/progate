fb47f84d64f7bb02c1462d74b10093b0f69b3bc0
k-kawaguchi-0124 <kouki.sekai124@gmail.com>
2025-09-11 12:47:51 +0900

    非同期処理が完了

--- asynchronous_promise_async/asynchronous/src/async_get_users.js
+++ asynchronous_promise_async/asynchronous/src/async_get_users.js
@@ -1,1 +1,38 @@
 const port = process.env.PORT || 8000;
+
+const fetchUser = (userId) => {
+    return fetch(`http://localhost:${port}/users/${userId}`)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("User not found");
+            }
+            return response.json();
+        })
+}
+
+// const main = async() => {
+//     try {
+//         for (let i = 1; i <= 6; i++) {
+//             await fetchUser(i)
+//                 .then((user) => {
+//                     console.log(user);
+//                     return fetchUser(i + 1);
+//                 })
+//         }
+//     } catch (err) {
+//         console.error(err.message);
+//     }
+// }
+
+const main = async() => {
+    try {
+        for (let i = 1; i <= 6; i++) {
+            const user = await fetchUser(i);
+            console.log(user);
+        }
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+main();
