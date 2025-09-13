2e14e4c6c7e0207e956c482a0b0a1e750f1951aa
k-kawaguchi-0124 <kouki.sekai124@gmail.com>
2025-09-13 18:18:16 +0900

    ブラウザでJavaScriptを実行してみようが完了

--- color_list_js_on_browser/color_list/public/main.js
+++ color_list_js_on_browser/color_list/public/main.js
@@ -1,3 +1,18 @@
-testElement = document.querySelector("#test");
+const testElement = document.querySelector("#test");
 testElement.innerHTML = "Good!"
 
+const ulElement = document.querySelector("#colors");
+
+const colors = ["blue", "orange", "green"];
+let ulText = "";
+for (let i = 0; i < colors.length; i++) {
+    const color = colors[i];
+    let backColor;
+    if (i % 2 === 0) {
+        backColor = "#EEE";
+    } else {
+        backColor = "#FFF";
+    }
+    ulText += `<li style="color: ${color}; background-color: ${backColor}">${color}</li>`;
+}
+ulElement.innerHTML = ulText;
